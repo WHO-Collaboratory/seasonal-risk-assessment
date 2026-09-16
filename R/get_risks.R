8c7d33787b470538e6e8d240753d30889728e9f5
Steve Kerr <steven.miles.k@gmail.com>
2026-09-16 13:59:03 +0200

    Support named weightings aligned to groupings
    Update get_risks to accept named weightings and align them with groupings. If weightings are named, verify names match names(groupings) and reorder weightings to the grouping order before normalization. Roxygen docs updated to explain the alignment behaviour (groupings may be reordered alphabetically by split).

--- R/get_risks.R
+++ R/get_risks.R
@@ -10,13 +10,21 @@
 #'   returned by \code{read_data}.
 #'
 #' @param weightings Numeric vector of the same length as \code{groupings}
-#'   indicating the relative weightings of the different pillars.
+#'   indicating the relative weightings of the different pillars. If named,
+#'   the names must match \code{names(groupings)} and are used to align the
+#'   two (since \code{groupings} is reordered alphabetically by \code{split}
+#'   and need not match the order \code{weightings} was supplied in).
 #'
 get_risks <- function(
   groupings,
   scores,
   weightings = rep(1 / length(groupings), length(groupings))
 ) {
+  if (!is.null(names(weightings))) {
+    stopifnot(setequal(names(weightings), names(groupings)))
+    weightings <- weightings[names(groupings)]
+  }
+
   weightings <- weightings / sum(weightings)
 
   tibble(
